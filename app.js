1ec2e5369959669f5ab0ae6ae21f643f0667ce22
Steven Eng <steveneng@Stevens-MacBook-Pro.local>
2017-08-24 02:12:31 -0400

    animated

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 $(document).ready(function(){
+    $('a').css('color','white')
    if($('a').hasClass('active')){
        $('a.active').css('color','#00b0eb')
    };
+   $('a').click(function(){
+       console.log('doge');
+       $('a.active').css('color','white')
+        $('a.active').removeClass('active');
+        $(this).addClass('active');
+        $(this).css('color','#00b0eb')
+        
+   });
    $(window).on('scroll',function(){
        if($(".splash").height()-30<$(window).scrollTop()){
-           console.log('doge');
            $('nav').css('background-color','#64F1EC')
        }
        else{
         $('nav').css('background-color','transparent')
        }
-   })
+   });
+   var title= ("{Frontend Developer}").split("");
+   var i=0;
+   setInterval(function(){
+       if(i<title.length){
+           $('.title').append(title[i]);
+           i++
+       }
+   },150);
+
+
 })
