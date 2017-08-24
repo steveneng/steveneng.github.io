4f04abac48e1296094916bf08bf54c4855626e2a
Steven Eng <steveneng@Stevens-MacBook-Pro.local>
2017-08-24 14:02:44 -0400

    better timed letters and css

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
    if($('a').hasClass('active')){
        $('a.active').css('color','#00b0eb')
    };
-   $('a').click(function(){
+   $('li>a').click(function(){
        console.log('doge');
        $('a.active').css('color','white')
         $('a.active').removeClass('active');
@@ -19,14 +19,33 @@ $(document).ready(function(){
         $('nav').css('background-color','transparent')
        }
    });
-   var title= ("{Frontend Developer}").split("");
+   var title= ("{Hi, I'm Steven}").split("");
+   var descriptions= ["I am a frontend developer","I am a coffee enthusiest","I am an owner of a lazy dog","I am a teacher","I am a mathematician","I am a guitarist","I am a vegan crossfitter","JK","Let's Talk"];
    var i=0;
+   var g=0;
    setInterval(function(){
        if(i<title.length){
            $('.title').append(title[i]);
            i++
        }
-   },150);
+   },80);
 
+   setInterval(function(){
+        if(g>=descriptions.length){
+            g=0;
+        }
+            $('.descriptions').empty();
+            $('.descriptions').append(descriptions[g]);
+            g++;
+            console.log(g)
+    },2000);
+
+   $('.arrow').hover(function () {
+        $(".pointer").css("opacity","1");
+        console.log('doge')
+       }, function () {
+        $(".pointer").css("opacity","0");
+       }
+   );
 
 })
